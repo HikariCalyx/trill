4a65db59b9e64cc7e7935c170c63e2fb5fe171e8
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-02-17 17:47:10 +0800

    fix icon

--- win/build.sh
+++ win/build.sh
@@ -15,6 +15,8 @@ tools/mako_generate.py "$(dirname "${BASH_SOURCE[0]}")/resource.rc.mako" >tango/
 mkdir Tango.iconset
 convert -resize 16x16 tango/src/icon.png -depth 32 Tango.iconset/icon_16x16.png
 convert -resize 32x32 tango/src/icon.png -depth 32 Tango.iconset/icon_32x32.png
+convert -resize 48x48 tango/src/icon.png -depth 32 Tango.iconset/icon_48x48.png
+convert -resize 64x64 tango/src/icon.png -depth 32 Tango.iconset/icon_64x64.png
 convert -resize 128x128 tango/src/icon.png -depth 32 Tango.iconset/icon_128x128.png
 convert -resize 256x256 tango/src/icon.png -depth 32 Tango.iconset/icon_256x256.png
 convert Tango.iconset/*.png tango/icon.ico
