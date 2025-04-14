1e91c2011c12aa3175bf4b1e25df9473abbd3cd3
Calyx Hikari <hikaricalyx@hikaricalyx.com>
2025-04-14 21:46:35 +0800

    2

--- tango/src/graphics/wgpu.rs
+++ tango/src/graphics/wgpu.rs
@@ -9,65 +9,79 @@ pub struct Backend {
     egui_winit: egui_winit::State,
     shapes: Vec<egui::epaint::ClippedShape>,
     textures_delta: egui::TexturesDelta,
-    ui_scale: f32,
 }
 
 impl Backend {
-    pub fn new<T>(
-        wb: winit::window::WindowBuilder,
-        event_loop: &winit::event_loop::EventLoopWindowTarget<T>,
+    pub fn new(
+        window_attributes: winit::window::WindowAttributes,
+        event_loop: &winit::event_loop::ActiveEventLoop,
     ) -> Result<Self, anyhow::Error> {
-        let window = Arc::new(wb.build(event_loop)?);
+        let window = Arc::new(event_loop.create_window(window_attributes)?);
 
-        let mut painter = egui_wgpu::winit::Painter::new(
+        let egui_ctx = egui::Context::default();
+
+        let painter = egui_wgpu::winit::Painter::new(
+            egui_ctx.clone(),
             egui_wgpu::WgpuConfiguration {
-                device_descriptor: Arc::new(|_| wgpu::DeviceDescriptor {
-                    label: None,
-                    required_features: wgpu::Features::default(),
-                    required_limits: wgpu::Limits {
-                        max_texture_dimension_2d: 4096,
-                        ..wgpu::Limits::downlevel_defaults()
+                wgpu_setup: egui_wgpu::WgpuSetup::CreateNew(egui_wgpu::WgpuSetupCreateNew {
+                    instance_descriptor: wgpu::InstanceDescriptor {
+                        backends: wgpu::Backends::PRIMARY | wgpu::Backends::GL,
+                        flags: wgpu::InstanceFlags::empty(),
+                        backend_options: wgpu::BackendOptions::default(),
                     },
+                    power_preference: wgpu::PowerPreference::LowPower,
+                    native_adapter_selector: None,
+                    device_descriptor: Arc::new(|_| wgpu::DeviceDescriptor {
+                        label: None,
+                        required_features: wgpu::Features::default(),
+                        required_limits: wgpu::Limits {
+                            max_texture_dimension_2d: 4096,
+                            ..wgpu::Limits::downlevel_defaults()
+                        },
+                        memory_hints: wgpu::MemoryHints::MemoryUsage,
+                    }),
+                    trace_path: None,
                 }),
-                supported_backends: wgpu::Backends::PRIMARY | wgpu::Backends::GL,
                 present_mode: wgpu::PresentMode::Fifo,
-                power_preference: wgpu::PowerPreference::LowPower,
                 ..Default::default()
             },
             1,
             None,
             false,
+            false,
         );
+
+        let mut painter = pollster::block_on(painter);
 
         pollster::block_on(painter.set_window(VIEWPORT, Some(window.clone())))?;
 
-        let egui_ctx = egui::Context::default();
-
-        let mut egui_winit = egui_winit::State::new(egui_ctx, VIEWPORT, event_loop, None, None);
-        egui_winit.egui_ctx().set_pixels_per_point(window.scale_factor() as f32);
+        let mut egui_winit = egui_winit::State::new(egui_ctx, VIEWPORT, event_loop, None, None, None);
         egui_winit.set_max_texture_side(painter.max_texture_side().unwrap_or(2048));
         let render_state = painter.render_state().unwrap();
         log::info!(
-            "wgpu device: {:?}, swapchain format: {:?}",
-            render_state.device,
+            "wgpu adapter: {:?}, swapchain format: {:?}",
+            render_state.adapter.get_info(),
             render_state.target_format
         );
+
         Ok(Self {
             window,
             painter,
             egui_winit,
             shapes: vec![],
             textures_delta: egui::TexturesDelta::default(),
-            ui_scale: 1.0,
         })
     }
 }
 
 impl graphics::Backend for Backend {
-    fn set_ui_scale(&mut self, scale: f32) {
-        self.ui_scale = scale;
-        self.egui_ctx()
-            .set_pixels_per_point(self.window.scale_factor() as f32 * self.ui_scale);
+    fn recreate_window(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_attributes: winit::window::WindowAttributes,
+    ) {
+        self.window = Arc::new(event_loop.create_window(window_attributes).unwrap());
+        pollster::block_on(self.painter.set_window(VIEWPORT, Some(self.window.clone()))).unwrap();
     }
 
     fn window(&self) -> &winit::window::Window {
@@ -85,7 +99,7 @@ impl graphics::Backend for Backend {
             [0.0, 0.0, 0.0, 1.0],
             &clipped_primitives,
             &std::mem::take(&mut self.textures_delta),
-            false,
+            Vec::new(),
         );
     }
 
@@ -93,7 +107,7 @@ impl graphics::Backend for Backend {
         self.egui_winit.egui_ctx()
     }
 
-    fn run(&mut self, run_ui: &mut dyn FnMut(&winit::window::Window, &egui::Context)) -> std::time::Duration {
+    fn run(&mut self, run_ui: &mut dyn FnMut(&egui::Context)) -> std::time::Duration {
         let egui::FullOutput {
             platform_output,
             textures_delta,
@@ -102,7 +116,7 @@ impl graphics::Backend for Backend {
             viewport_output,
         } = {
             let egui_input = self.egui_winit.take_egui_input(&self.window);
-            self.egui_ctx().run(egui_input, |ui| run_ui(&self.window, ui))
+            self.egui_ctx().run(egui_input, run_ui)
         };
 
         self.egui_winit.handle_platform_output(&self.window, platform_output);
@@ -114,24 +128,12 @@ impl graphics::Backend for Backend {
     }
 
     fn on_window_event(&mut self, event: &winit::event::WindowEvent) -> egui_winit::EventResponse {
-        match event {
-            winit::event::WindowEvent::Resized(physical_size) => {
-                if let (Ok(width), Ok(height)) = (physical_size.width.try_into(), physical_size.height.try_into()) {
-                    self.painter.on_window_resized(VIEWPORT, width, height);
-                }
+        if let winit::event::WindowEvent::Resized(physical_size) = event {
+            if let (Ok(width), Ok(height)) = (physical_size.width.try_into(), physical_size.height.try_into()) {
+                self.painter.on_window_resized(VIEWPORT, width, height);
             }
-            winit::event::WindowEvent::ScaleFactorChanged { scale_factor, .. } => {
-                self.egui_ctx()
-                    .set_pixels_per_point(*scale_factor as f32 * self.ui_scale);
-
-                let inner_size = self.window.inner_size();
-
-                if let (Ok(width), Ok(height)) = (inner_size.width.try_into(), inner_size.height.try_into()) {
-                    self.painter.on_window_resized(VIEWPORT, width, height);
-                }
-            }
-            _ => {}
         }
+
         self.egui_winit.on_window_event(&self.window, event)
     }
 }
